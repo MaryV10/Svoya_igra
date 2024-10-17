8bd6f808aae2276f9e303d2ec15c59f837103d04
Nikita <felasso3212@gmail.com>
2024-10-17 18:08:22 +0300

    Create Crud Game

--- server/src/routers/api.router.js
+++ server/src/routers/api.router.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const authRouter = require('./auth.api.router');
+const gameRouter = require('./game.router');
 const topicRouter = require('./topic.router');
 
 
 
 router.use('/auth', authRouter);
 router.use('/cards', topicRouter);
+router.use('/games', gameRouter);
 
 
 
