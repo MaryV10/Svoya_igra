34f6425410762a93db5972abe8fad9f3afa0a261
MaryV10 <mary10@list.ru>
2024-10-18 12:19:06 +0300

    fixed-bugs

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { ROUTES } from '@/app/router/routes';
+import { ThemeButton } from '@/shared/ui/Button/Button';
+import { Button } from 'antd';
+import styles from './HomePage.module.css'
+import { Link } from 'react-router-dom';
 
 export const HomePage: React.FC = () => {
-  return <h1>Home Page</h1>;
+  return (       
+  <>
+  <p>HOME PAGE</p>
+  <Button>
+  {/* <Link to={ROUTES.HOME}>Квиз</Link> */}
+</Button>
+</>)
 };
