f6da5e374e2558f7866510011574a8fb6a588801
Andrey Veselov <lifeforglory@gmail.com>
2024-10-18 11:14:16 +0300

    feat:game entity done

--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import { userSlice } from '@/entities/user';
+import { gameSlice } from '@/entities/game';
 
 
 export const store = configureStore({
   reducer: {
     
     user: userSlice,
+    game: gameSlice,
     
   },
 });
---
+++ client/src/entities/game/model/gameSlice.ts
@@ -0,0 +1,87 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Game}  from ".";
+
+import { createGame, getGame, updateScoreGame, updateStatusGame } from "../api/gameThunks";
+
+type GameSliceType = {
+  game: Game[];
+  error: string | null;
+  loading: boolean;
+};
+
+const initialState: GameSliceType = {
+    game: [],
+  error: null,
+  loading: false,
+};
+
+const gameSlice = createSlice({
+  name: "game",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getGame.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getGame.rejected, (state, action) => {
+        state.error = action.error.message || "Error to get game";
+      })
+      .addCase(getGame.fulfilled, (state, action) => {
+       
+        state.loading = false;
+        state.error = null;
+        state.game = [action.payload];
+      })
+
+      //!----------------------- CREATE
+      .addCase(createGame.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(createGame.rejected, (state, action) => {
+        state.error = action.error.message || "Error to get tasks";
+      })
+      .addCase(createGame.fulfilled, (state, action) => {
+        
+        state.loading = false;
+        state.error = null;
+        state.game = [action.payload];
+      })
+      //!----------------------- UPD STATUS
+      .addCase(updateStatusGame.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(updateStatusGame.rejected, (state, action) => {
+        state.error = action.error.message || "Error to get tasks";
+      })
+      .addCase(updateStatusGame.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.game = [action.payload];
+        // state.tasks = state.tasks.map((task) =>
+        //   task.id === action.payload.id ? action.payload : task
+        // );
+      })
+
+
+
+
+      //!----------------------- UPD SCORE
+      .addCase(updateScoreGame.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(updateScoreGame.rejected, (state, action) => {
+        state.error = action.error.message || "Error to get tasks";
+      })
+      .addCase(updateScoreGame.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.game = [action.payload];
+        // state.tasks = state.tasks.map((task) =>
+        //   task.id === action.payload.id ? action.payload : task
+        // );
+      })
+  },
+});
+
+export default gameSlice.reducer;
