8bdb840d28591613ca8df2e3d5f5916e52d15a68
Dmitry <dmi.pol@icloud.com>
2024-10-18 08:59:36 +0300

    cards fix

--- client/src/widgets/CardList/CardList.tsx
+++ client/src/widgets/CardList/CardList.tsx
@@ -7,24 +7,30 @@ import { useEffect, useState } from 'react';
 export const CardList: React.FC = () => {
 
   const [cards , setCards] = useState([])
+// const { Cards } =  cards
 
   useEffect(() => {
     async function getAll() {
       const fetchedCards = await CardService.getAllCards();
-      console.log(fetchedCards)
       setCards(fetchedCards); // Update the state with fetched cards
+
     }
     getAll();
   }, []); 
+  console.log(cards.Cards)
+  
+  const allCards = cards.flatMap((card) => card.Cards || []);
 
-  console.log(cards)
+ 
   return (
     <div>
       
-      {cards.map((question) => (
+      {allCards.map((card) => (
+       
         <CardItem
-          key={question.id}
-          question={question}
+          key={card.id}
+          card={card}
+        
         />
       ))}
     </div>
